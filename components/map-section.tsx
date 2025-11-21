75d31de815c08a2055d5ff0cd0bab942d23d1622
GaragesWirral <newsletterproductoffers@gmail.com>
2025-11-21 19:34:12 +0000

    Update map-section.tsx

--- components/map-section.tsx
+++ components/map-section.tsx
@@ -13,7 +13,7 @@ export function MapSection() {
         <div className="max-w-5xl mx-auto">
           <div className="aspect-video rounded-2xl overflow-hidden shadow-2xl border-4 border-white">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d366.4246757678826!2d-1.6194430536227902!3d53.807511797642015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48795f321f88b3f9%3A0x6729978e353423d7!2s80%20Aston%20Terrace%2C%20Leeds%20LS13%202EB%2C%20UK!5e1!3m2!1sen!2ses!4v1763752791743!5m2!1sen!2ses"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1000!2d-1.6194430!3d53.8075118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48795f321f88b3f9%3A0x6729978e353423d7!2s80%20Aston%20Terrace%2C%20Leeds%20LS13%202EB%2C%20UK!5e0!3m2!1sen!2suk!4v1700000000000"
               width="100%"
               height="100%"
               style={{ border: 0 }}
